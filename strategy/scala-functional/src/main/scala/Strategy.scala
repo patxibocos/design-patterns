be2ca59e032c5dd905c91d993c6541b59abf5ec1
Patxi Bocos <patxi952@gmail.com>
2016-10-12 22:09:05 +0200

    Strategy pattern with super heroes example.

--- strategy/scala-functional/src/main/scala/Strategy.scala
+++ strategy/scala-functional/src/main/scala/Strategy.scala
@@ -1,33 +1,33 @@
 object Strategy {
 
-  type SayHello = () => String
+  type Ability = () => String
 
-  def sayHelloImpl1 = () => "Hello1!"
-  def sayHelloImpl2 = () => "Hello2!"
-  def sayHelloImpl3 = () => "Hello3!"
+  def flyability: Ability = () => "I can fly like a bird"
+  def invulnerability: Ability = () => "I´m invulnerable, try to hurt me!"
+  def invisibility: Ability = () => "Can you see me?"
 
-  abstract class Duck {
-    // This will allow us calling greet without a method as a child class could have overwritten this with a default value (PlasticDuck example)
-    def greet(sayHello: Option[SayHello]) = sayHello match {
-      case Some(callback) => callback()
+  abstract class SuperHero {
+    // This will allow us calling greet without a method as a child class could have overwritten this with a default value (Venom example)
+    def performAbility(ability: Option[Ability]) = ability match {
+      case Some(callback) => println(s"${this.getClass.getSimpleName} says: ${callback()}")
       case None => None
     }
   }
 
-  case class NormalDuck() extends Duck
+  case class Superman() extends SuperHero
 
-  case class PlasticDuck() extends Duck {
+  case class Venom() extends SuperHero {
     // Call parents with a default function
-    override def greet(sayHello: Option[SayHello]) = super.greet(Some(sayHello.getOrElse(sayHelloImpl2)))
+    override def performAbility(ability: Option[Ability]) = super.performAbility(Some(ability.getOrElse(invisibility)))
   }
 
   def main(args: Array[String]): Unit = {
-    val normalDuck: Duck = NormalDuck()
-    System.out.println(normalDuck.greet(Some(sayHelloImpl1)))
+    val superman: SuperHero = Superman()
+    superman.performAbility(Some(flyability))
 
-    val plasticDuck: Duck = PlasticDuck()
-    System.out.println(plasticDuck.greet(None))
-    System.out.println(plasticDuck.greet(Some(sayHelloImpl3)))
+    val venom: SuperHero = Venom()
+    venom.performAbility(None)
+    venom.performAbility(Some(invulnerability))
   }
 
 }
